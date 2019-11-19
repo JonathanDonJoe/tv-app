5894886e1cb2a90aa6e39276f8541cc5b2ead8e4
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-11-19 13:52:54 -0500

    Added helmet and CORS check

--- back-end/app.js
+++ back-end/app.js
@@ -2,11 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'public/index.html'), function(err) {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     })
+//   })
 
 module.exports = app;
