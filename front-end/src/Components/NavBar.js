afde7edb5b688639bab1f8642c7b448208e4121b
Nathan Evans <nevans.dev@gmail.com>
2019-11-19 23:04:01 -0500

    Set up the component NavBar.

---
+++ front-end/src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                Sanity Check
+            </div>
+        )
+    }
+}
+
+export default NavBar
